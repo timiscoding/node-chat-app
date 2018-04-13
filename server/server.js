3ee9c3aa81a200a60a869a8d4ae2fc87c63ca480
Tim <timiscoding@gmail.com>
2018-04-13 18:21:04 +1000

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ const io = socketIO(server);
 io.on('connection', socket => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime(),
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined the chat',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', message => {
     console.log('Create message', message);
     io.emit('newMessage', {
@@ -20,6 +31,12 @@ io.on('connection', socket => {
       text: message.text,
       createdAt: new Date().getTime(),
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime(),
+    // });
   });
 
   socket.on('disconnect', socket => {
