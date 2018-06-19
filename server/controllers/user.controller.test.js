429e7f210164edd737bd01c3400761471c9b04e4
Tim <timiscoding@gmail.com>
2018-06-19 16:04:06 +1000

    Add invalid form data tests for /POST signup

--- server/controllers/user.controller.test.js
+++ server/controllers/user.controller.test.js
@@ -36,7 +36,7 @@ describe.only('POST /signup', () => {
   });
 
   it('should not create a new user with same email', async () => {
-    await User.create(user);
+    await User.create({ ...user, username: 'not same username' });
 
     const res = await chai.request(server)
       .post('/signup')
@@ -47,5 +47,55 @@ describe.only('POST /signup', () => {
     expect(res).to.not.redirect;
     const users = await User.find({ email: user.email });
     expect(users).to.have.length(1);
+  });
+
+  it('should not create a new user with same username', async () => {
+    await User.create({ ...user, email: 'someotheremail@example.com' });
+
+    const res = await chai.request(server)
+      .post('/signup')
+      .type('form')
+      .send(user);
+
+    expect(res).to.have.status(200);
+    expect(res).to.not.redirect;
+    const users = await User.find({ username: user.username });
+    expect(users).to.have.length(1);
+  });
+
+  it('should not create a new user without email', async () => {
+    const res = await chai.request(server)
+      .post('/signup')
+      .type('form')
+      .send({ ...user, email: '' });
+
+    expect(res).to.have.status(200);
+    expect(res).to.not.redirect;
+    const users = await User.find();
+    expect(users).to.have.length(0);
+  });
+
+  it('should not create a new user without username', async () => {
+    const res = await chai.request(server)
+      .post('/signup')
+      .type('form')
+      .send({ ...user, username: '' });
+
+    expect(res).to.have.status(200);
+    expect(res).to.not.redirect;
+    const users = await User.find();
+    expect(users).to.have.length(0);
+  });
+
+  it('should not create a new user if password confirmation does not match password field', async () => {
+    const res = await chai.request(server)
+      .post('/signup')
+      .type('form')
+      .send({ ...user, 'password-confirm': '321cba' });
+
+    expect(res).to.have.status(200);
+    expect(res).to.not.redirect;
+    const users = await User.find();
+    expect(users).to.have.length(0);
   });
 });
