6ce668dd16ab123b464e8125ebb987a74fdd96b4
Tim <timiscoding@gmail.com>
2018-08-07 12:08:04 +1000

    Add mongo session store for prod

--- server/middleware.js
+++ server/middleware.js
@@ -4,12 +4,29 @@ import bodyParser from 'body-parser';
 import passport from 'passport';
 import session from 'express-session';
 import flash from 'connect-flash';
+import mongoose from 'mongoose';
+import connectMongo from 'connect-mongo';
+
+const MongoStore = connectMongo(session);
+
+const sessionConfig = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.NODE_ENV === 'production') {
+  sessionConfig.store = new MongoStore({
+    mongooseConnection: mongoose.connection,
+    autoRemove: 'native',
+  });
+}
 
 export default [
   express.static(path.join(__dirname, '../public')),
   bodyParser.json(),
   bodyParser.urlencoded({ extended: true }),
-  session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }),
+  session(sessionConfig),
   passport.initialize(),
   passport.session(),
   flash(),
